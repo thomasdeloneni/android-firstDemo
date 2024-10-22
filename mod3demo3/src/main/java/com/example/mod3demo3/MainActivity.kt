c77966106abec9b0b14fd7bb8f6e91dab33bcf0c
thomasdeloneni <149668246+thomasdeloneni@users.noreply.github.com>
2024-10-22 15:56:48 +0200

    xml mod3demo3

--- mod3demo3/src/main/java/com/example/mod3demo3/MainActivity.kt
+++ mod3demo3/src/main/java/com/example/mod3demo3/MainActivity.kt
@@ -4,44 +4,64 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.mod3demo3.ui.theme.FirstDemoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FirstDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun RowAndColumnExample(){
+    Column {
+        Text("Example pour Row et column")
+        Row {
+            Text("Element 1")
+            Text("Element 2")
+            Text("Element 3")
+        }
+        Column {
+            Text("Item 1")
+            Text("Item 2")
+            Text("Item 3")
+        }
+    }
+}
+
+@Composable
+fun BoxExample() {
+
+    Box(contentAlignment = Alignment.BottomEnd) {
+
+        Image(painter = painterResource(id=R.drawable.compose_man) , contentDescription = "Compose Man")
+
+        Icon(
+            imageVector = Icons.Default.Face,
+            contentDescription=null,
+            tint = Color.Yellow
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    FirstDemoTheme {
-        Greeting("Android")
-    }
+fun Preview() {
+    BoxExample()
 }
